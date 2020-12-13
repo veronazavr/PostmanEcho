4ec16e35272fd7330853ca9a7e8de35bb0d61a06
Veronazavr <verochka10@list.ru>
2020-12-13 19:18:20 +0300

    added updated

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -2,9 +2,22 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PostmanEchoTest {
     @Test
     void shouldReturnTestData () {
-        
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("PostmanEchoTest data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("PostmanEchoTest data"))
+        ;
     }
-}
+
+    }
+
