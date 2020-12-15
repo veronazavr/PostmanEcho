785499b4e78d15f83c4fbc79c3d22906641db4c9
Veronazavr <verochka10@list.ru>
2020-12-16 00:11:27 +0300

    added updated

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -15,7 +15,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("PostmanEchoTest data"))
+                .body("data", equalTo("PostmanEchoTes"))
         ;
     }
 
